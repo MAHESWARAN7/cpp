82beef4b75218282e89bed3d67b5952e283c61f7
MAHESWARAN7 <smaheswaran7002@gmail.com>
2026-02-15 17:51:37 +0530

    hi cpp

--- first.cpp
+++ first.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Hello, World!";
+
+    int phno;
+    const int dob = 2007 ;
+
+
+    cout << "Hello \a \", World!\" "<< endl;
+    cout<< "i am "<< 2026-2007<<" years old"<<endl;
+    std::cout<< "phno is "<<1134456 << "\n my dob is "<<dob ;
+    
     return 0;
 }
