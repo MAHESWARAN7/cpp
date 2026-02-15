90d39389d4eaa756f14bb82b86133b4595575af0
MAHESWARAN7 <smaheswaran7002@gmail.com>
2026-02-15 19:20:00 +0530

    f

--- first.cpp
+++ first.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main(){
 int phno= 861032;
 const int dob = 20070107;
-char name = mahes;
+char name = 'm';
+float cgpa;
+double pi ;
+
+
+
+cout<<"enter ur cgpa: ";
+cin>>cgpa;
+cout<< "u r cgpa is "<<cgpa<<endl;
+
+cout<<"enter value of pi : ";
+cin>>pi;
+cout<<fixed<<setprecision(10);
+cout<<pi<<endl;
+
     
 
     cout << "Hello \a \", World!\" "<< endl;
-    cout<< "i am "<< name << 2026-2007<<" years old"<<endl;
+    cout<< "i  "<< name  <<  2026-2007<<" years old"<<endl;
     std::cout<< "phno is "<<phno << "\n my dob is "<<dob ;
     
     return 0;
