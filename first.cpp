8e06ec1f21d333514489066fb5426e736cbcdc2d
MAHESWARAN7 <smaheswaran7002@gmail.com>
2026-02-15 18:37:16 +0530

    Fix variable declaration and update output messagename

--- first.cpp
+++ first.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(){
 int phno= 861032;
 const int dob = 20070107;
-    
+char name = mahes;
     
 
     cout << "Hello \a \", World!\" "<< endl;
-    cout<< "i am "<< 2026-2007<<" years old"<<endl;
+    cout<< "i am "<< name << 2026-2007<<" years old"<<endl;
     std::cout<< "phno is "<<phno << "\n my dob is "<<dob ;
     
     return 0;
