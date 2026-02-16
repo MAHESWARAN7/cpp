d9ab4abe66e025776e3a03274ad065881947fbe4
MAHESWARAN7 <smaheswaran7002@gmail.com>
2026-02-16 18:45:30 +0530

    new

--- first.cpp
+++ first.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
 #include <iomanip>
+#include<string>
+
+
 using namespace std;
+
+
 int main(){
+
+
 int phno= 861032;
 const int dob = 20070107;
-char name = 'm';
+char letter = 65;
 float cgpa;
 double pi ;
-
-
+string name = "mahes waran";
+bool yes = true;
 
 cout<<"enter ur cgpa: ";
 cin>>cgpa;
-cout<< "u r cgpa is "<<cgpa<<endl;
 
 cout<<"enter value of pi : ";
 cin>>pi;
 cout<<fixed<<setprecision(10);
-cout<<pi<<endl;
+
+cout<< "u r cgpa is "<<cgpa<<endl;
+cout<<"value : "<<pi<<endl;
 
     
 
     cout << "Hello \a \", World!\" "<< endl;
-    cout<< "i  "<< name  <<  2026-2007<<" years old"<<endl;
-    std::cout<< "phno is "<<phno << "\n my dob is "<<dob ;
+    cout<< "i  "<< letter  <<  2026-2007<<" years old"<<endl;
+    std::cout<< "phno is "<<phno << "\n my dob is "<<dob<<"\n i am "<<name<<endl ;
+    cout<<yes;
     
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char name[53];
+    cin>>name;
+    cout<< "uname:"<<name; 
+}
